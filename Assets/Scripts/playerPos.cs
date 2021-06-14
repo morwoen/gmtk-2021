bb187792eff3763a4e567c2749dbb8e4d8b0e601
Georgi Tsaklev <georgi.tsaklev@gmail.com>
2021-06-14 10:15:26 +0100

    Fix bug with checkpoint respawning where Igni was not being teleported

--- Assets/Scripts/playerPos.cs
+++ Assets/Scripts/playerPos.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class playerPos : MonoBehaviour
-{   //This script need to be attached to both players 
-    // Start is called before the first frame update 
+{
+  // This script need to be attached to the parent object of both players
+
   private gameMaster gm;
   void Start() {
     MovePlayer();
@@ -13,6 +14,23 @@ public class playerPos : MonoBehaviour
 
   public void MovePlayer() {
     gm = GameObject.FindGameObjectWithTag("GM").GetComponent<gameMaster>();
+
+    CharacterController enabledOne = null;
+
+    var playerCC = transform.GetChild(0).GetComponent<CharacterController>();
+    if (playerCC.enabled) {
+      enabledOne = playerCC;
+      playerCC.enabled = false;
+    }
+    playerCC = transform.GetChild(1).GetComponent<CharacterController>();
+    if (playerCC.enabled) {
+      enabledOne = playerCC;
+      playerCC.enabled = false;
+    }
+
     transform.position = gm.lastCheckpointPos;
+    if (enabledOne != null) {
+      enabledOne.enabled = true;
+    }
   }
 }
